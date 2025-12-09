0f1c71ce0c0f41212b68b68aa1b5faf3711943ef
Osakana3610 <fishnchips.me@gmail.com>
2025-12-10 03:01:51 +0900

    Refactor: StackKeyComponentsにstackKey生成メソッドを追加
    Phase 15-1完了:
    - StackKeyComponentsにstackKeyプロパティとmakeStackKey静的メソッドを追加
    - memberwise initを追加
    - 既存のstackKey生成の重複を解消するための基盤を整備

--- Epika/Progress/Persistence/Models/StackKeyComponents.swift
+++ Epika/Progress/Persistence/Models/StackKeyComponents.swift
@@ -9,6 +9,40 @@ struct StackKeyComponents: Sendable {
     let socketNormalTitleId: UInt8
     let socketItemId: UInt16
 
+    /// コンポーネントからstackKeyを生成
+    var stackKey: String {
+        "\(superRareTitleId)|\(normalTitleId)|\(itemId)|\(socketSuperRareTitleId)|\(socketNormalTitleId)|\(socketItemId)"
+    }
+
+    /// 指定された値からstackKeyを生成（静的メソッド）
+    static func makeStackKey(
+        superRareTitleId: UInt8,
+        normalTitleId: UInt8,
+        itemId: UInt16,
+        socketSuperRareTitleId: UInt8 = 0,
+        socketNormalTitleId: UInt8 = 0,
+        socketItemId: UInt16 = 0
+    ) -> String {
+        "\(superRareTitleId)|\(normalTitleId)|\(itemId)|\(socketSuperRareTitleId)|\(socketNormalTitleId)|\(socketItemId)"
+    }
+
+    /// memberwise init
+    init(
+        superRareTitleId: UInt8,
+        normalTitleId: UInt8,
+        itemId: UInt16,
+        socketSuperRareTitleId: UInt8,
+        socketNormalTitleId: UInt8,
+        socketItemId: UInt16
+    ) {
+        self.superRareTitleId = superRareTitleId
+        self.normalTitleId = normalTitleId
+        self.itemId = itemId
+        self.socketSuperRareTitleId = socketSuperRareTitleId
+        self.socketNormalTitleId = socketNormalTitleId
+        self.socketItemId = socketItemId
+    }
+
     /// stackKey文字列をパースして各コンポーネントを抽出
     /// フォーマット: "superRareTitleId|normalTitleId|itemId|socketSuperRareTitleId|socketNormalTitleId|socketItemId"
     nonisolated init?(stackKey: String) {
